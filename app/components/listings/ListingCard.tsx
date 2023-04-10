bc6221ad2e4f81ea3bec75f019f16bfb007390a6
AdrianOles <olesnieadrian@gmail.com>
2023-04-10 14:45:05 -0400

    why not work

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -14,7 +14,6 @@ import {
 
 import HeartButton from "../HeartButton";
 import Button from "../Button";
-import ClientOnly from "../ClientOnly";
 
 interface ListingCardProps {
     data: SafeListing;
@@ -38,7 +37,6 @@ const ListingCard: React.FC<ListingCardProps> = ({
     const router = useRouter();
     const { getByValue } = useCountries();
 
-    //@ts-ignore
     const location = getByValue(data.locationValue);
 
     const handleCancel = useCallback(
@@ -79,30 +77,30 @@ const ListingCard: React.FC<ListingCardProps> = ({
             <div className="flex flex-col gap-2 w-full">
                 <div
                     className="
-            aspect-square 
-            w-full 
-            relative 
-            overflow-hidden 
-            rounded-xl
-          "
+                        aspect-square 
+                        w-full 
+                        relative 
+                        overflow-hidden 
+                        rounded-xl
+                    "
                 >
                     <Image
                         fill
                         className="
-              object-cover 
-              h-full 
-              w-full 
-              group-hover:scale-110 
-              transition
-            "
+                                object-cover 
+                                h-full 
+                                w-full 
+                                group-hover:scale-110 
+                                transition
+                                "
                         src={data.imageSrc}
                         alt="Listing"
                     />
                     <div className="
-            absolute
-            top-3
-            right-3
-          ">
+                            absolute
+                            top-3
+                            right-3
+                        ">
                         <HeartButton
                             listingId={data.id}
                             currentUser={currentUser}
